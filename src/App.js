f0041e875ce4f61a45a7d48255b22b8881305403
lautiibarreras <l.barreras@alumno.etec.um.edu.ar>
2024-09-13 09:12:50 -0300

    Token y verificacion

--- src/App.js
+++ src/App.js
@@ -78,7 +78,7 @@ const App = () => {
                 {/* Navbar */}
                 <nav className="navbar navbar-expand-lg navbar-light bg-light">
                     <div className="container-fluid">
-                        <a className="navbar-brand" href="#">App</a>
+                        <a className="navbar-brand" href="#">MayBa</a>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
